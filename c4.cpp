616c3ddc90bf8cdd3408dd12b2411163e6daea8e
hai113114 <phamngochai3003@gmail.com>
2024-11-27 16:08:17 +0700

    c4.cpp

--- c4.cpp
+++ c4.cpp
@@ -9,18 +9,11 @@ using namespace std;
 using namespace tesseract;
 
 int main() {
-    // Đường dẫn ảnh và tệp văn bản
     string image_path = "D:/Download/binary_image.png";
     string output_text_path = "D:/VSC/ticket_full_text.txt";
-
-    // Đọc ảnh và chuyển sang ảnh xám
     Mat image = imread(image_path, IMREAD_GRAYSCALE);
-
-    // Tăng cường ảnh (chuyển sang ảnh nhị phân và tăng độ tương phản)
     Mat binary;
     threshold(image, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
-
-    // Thực hiện OCR trên ảnh nhị phân với cấu hình cải tiến
     TessBaseAPI ocr;
     if (ocr.Init(NULL, "por", OEM_LSTM_ONLY) != 0) {
         cerr << "Không thể khởi tạo Tesseract OCR!" << endl;
@@ -29,11 +22,8 @@ int main() {
 
     ocr.SetPageSegMode(PSM_SINGLE_BLOCK);
     ocr.SetImage(binary.data, binary.cols, binary.rows, 1, binary.step);
-
-    // Nhận diện văn bản
     string full_text = ocr.GetUTF8Text();
 
-    // Lưu văn bản nhận dạng vào tệp
     ofstream outFile(output_text_path);
     if (outFile.is_open()) {
         outFile << full_text;
